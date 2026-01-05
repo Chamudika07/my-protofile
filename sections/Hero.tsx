44de08fe58c123f9e116b4d6c3a5b60bd83ebea2
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2026-01-05 09:48:34 +0530

    change somthing

--- sections/Hero.tsx
+++
@@ -1,8 +0,0 @@
-  <Image
-    src="/images/profile_photo.jpeg"
-    alt="Chamudika Pramod Ariyasena"
-    width={192}
-    height={192}
-    className="object-cover w-full h-full"
-    priority
-  /> 
